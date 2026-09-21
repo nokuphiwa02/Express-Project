1d9aec620b9d23d1bea3337842c751b6f95fc79d
Nokuphiwa <shangenokuphiwa683@gmail.com>
2026-09-21 09:07:17 +0200

    books router

--- src/routes/Author.ts
+++ src/routes/Author.ts
@@ -1,41 +1,60 @@
-import {Router, Request, Response } from "express";
-import {body, param, validationResult} from "express-validator";
-import {getAllAuthors, getAuthorsById , createAuthor} from "../controllers/Author"
+import { Router, Request, Response } from "express";
+import { body, param, validationResult } from "express-validator";
+import {
+  getAllAuthors,
+  getAuthorsById,
+  getBooksByAuthor,
+  createAuthor,
+} from "../controllers/Author";
 import { Authors } from "../models";
 
-const router = Router()
+const router = Router();
 
-let authors = []
+// let authors = []
 
-router.get("/",getAllAuthors) 
-    
+router.get("/", getAllAuthors);
 
+router.get(
+  "/:id/books",
+  [param("id").isInt().withMessage("Author ID must be an integer")],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } //getting books by author Id
+    getBooksByAuthor(req, res);
+  },
+);
 
 //http://localhost:3000/:id
-router.get("/:id",
-    [param("id").isInt().withMessage("ID must be an integer")],
-    (req: Request, res: Response) => {
-    const errors = validationResult(req)
+router.get(
+  "/:id",
+  [param("id").isInt().withMessage("ID must be an integer")],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
     console.log(errors, "errors from express-validator middleware");
 
-    if(!errors.isEmpty()){
-    return res.status(400).json({errors: errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-     getAuthorsById(req, res)
-})
+    getAuthorsById(req, res);
+  },
+);
 
-  router.post("/",[
+router.post(
+  "/",
+  [
     body("name").notEmpty().withMessage("Name is required"),
     body("email").isEmail().withMessage("Must be a valid email address"),
-],
-    (req: Request, res: Response) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.array() })
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-     createAuthor(req, res)
-});
+    createAuthor(req, res);
+  },
+);
 export default router;
-
--- src/routes/Book.ts
+++ src/routes/Book.ts
@@ -1,42 +1,44 @@
-import {Router, Request, Response } from "express";
-import {body, param, validationResult} from "express-validator";
-import {getAllBooks, getBooksById ,createBook} from "../controllers/Book"
+import { Router, Request, Response } from "express";
+import { body, param, validationResult } from "express-validator";
+import { getAllBooks, getBooksById, createBook } from "../controllers/Book";
 import { Books } from "../models";
 
-const router = Router()
+const router = Router();
 
-let books = []
-
-router.get("/",getAllBooks) 
-    
-
+router.get("/", getAllBooks);
 
 //http://localhost:3000/:id
-router.get("/:id",
-    [param("id").isInt().withMessage("ID must be an integer")],
-    (req: Request, res: Response) => {
-    const errors = validationResult(req)
+router.get(
+  "/:id",
+  [param("id").isInt().withMessage("ID must be an integer")],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
     console.log(errors, "errors from express-validator middleware");
 
-    if(!errors.isEmpty()){
-    return res.status(400).json({errors: errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    getBooksById(req, res)
-})
+    getBooksById(req, res);
+  },
+);
 
-  router.post("/",[
+router.post(
+  "/",
+  [
     body("title").notEmpty().withMessage("Title is required"),
-    body("author").notEmpty().withMessage("Author is requireds"),
-    body("year") .isISO8601().withMessage("Year must be a valid date format (YYYY-MM-DD)")
-],
-    (req: Request, res: Response) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.array() })
+    body("authorId")
+      .isInt()
+      .withMessage("Author ID is required and must be an integer"),
+    body("year").isInt().withMessage("Year must be a valid number "),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-     createBook(req, res)
-});
+    createBook(req, res);
+  },
+);
 export default router;
-
