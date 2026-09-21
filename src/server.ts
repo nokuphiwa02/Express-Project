65410fc0a4154db46308dbe1b58928b2f1f3b302
Nokuphiwa <shangenokuphiwa683@gmail.com>
2026-09-21 09:26:25 +0200

    update server

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,19 @@
-import { Router, Request, Response } from "express";
-import { body, param, validationResult } from "express-validator";
-import { getAllBooks, getBooksById, createBook } from "../controllers/Book";
-import { Books } from "../models";
+import bodyParser from "body-parser";
+import express, { Express } from "express";
+import { looggerMiddleware } from "./middleware/logger";
+import authorRouter from "./routes/Author";
+import bookRouter from "./routes/Book";
 
-const router = Router();
+const app: Express = express();
+const PORT = process.env.PORT || 3000;
 
-router.get("/", getAllBooks);
+app.use(express.json());
+app.use(bodyParser.json());
 
-//http://localhost:3000/:id
-router.get(
-  "/:id",
-  [param("id").isInt().withMessage("ID must be an integer")],
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
+app.use(looggerMiddleware);
+app.use("/Authors", authorRouter);
+app.use("/Books", bookRouter);
 
-    console.log(errors, "errors from express-validator middleware");
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    getBooksById(req, res);
-  },
-);
-
-router.post(
-  "/",
-  [
-    body("title").notEmpty().withMessage("Title is required"),
-    body("authorId")
-      .isInt()
-      .withMessage("Author ID is required and must be an integer"),
-    body("year").isInt().withMessage("Year must be a valid number "),
-  ],
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    createBook(req, res);
-  },
-);
-export default router;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
