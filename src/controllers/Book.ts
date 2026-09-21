e5c6e4fad7578a96609177ad7eacfa0a95a1f9d8
Nokuphiwa <shangenokuphiwa683@gmail.com>
2026-09-21 08:18:26 +0200

    get books by id

--- src/controllers/Book.ts
+++ src/controllers/Book.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import { Books } from "../models";
+import { authors } from "../controllers/Author";
 
-let books: Books[] = [];
+export let books: Books[] = [];
 
 export const getAllBooks = (req: Request, res: Response) => {
   res.status(200).json(books);
@@ -12,15 +13,15 @@ export const getBooksById = (req: Request, res: Response) => {
   const book = books.find((book) => book.id === parseInt(id as string));
 
   if (!book) {
-    return res.status(404).send("user not found");
+    return res.status(404).send("Book not found");
   }
 
   res.status(200).json(book);
 };
 
 export const createBook = (req: Request, res: Response) => {
-  const { title, author, year } = req.body;
-  const newBook = { id: books.length + 1, title, author, year };
+  const { title, authorId, year } = req.body;
+  const newBook = { id: books.length + 1, title, authorId, year };
 
   books.push(newBook);
 
